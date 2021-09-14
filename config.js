08af2df40091e5dd23d7be2d8d6c465ea71b2e94
hoichoi <ccaaii@qq.com>
2021-09-14 17:03:37 +0800

    gogo

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    title: "Title",  //站点名
+    downloadLink_h5: "http://www.baidu.com", //H5端下载链接
+    downloadLink_pc: "http://www.baidu.com", //PC端下载链接
+    qq:["1234564","78946646"],  //QQ号码
+    kefuLink_h5: "http://www.baidu.com", //H5端客服链接
+    kefuLink_pc: "http://www.baidu.com", //PC端客服链接
+    countJs: "" //统计代码链接
+}
