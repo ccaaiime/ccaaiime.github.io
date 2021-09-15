7585aef334aa796d23d2a3bec8a16885b3711571
hoichoi <ccaaii@qq.com>
2021-09-15 23:31:58 +0800

    H5

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var config = {
-    title: "Title1",  //站点名
-    downloadLink_h5: "http://www.baidu.com", //H5端下载链接
-    downloadLink_pc: "http://www.baidu.com", //PC端下载链接
+    title: "GOGO体育携手亚博体育共享体育盛世",  //站点名
+    downloadLink_h5: ["http://www.gogo.com","http://www.yb.com"], //H5端下载链接
+    regUrl_pc: ["http://www.gogo.com","http://www.yb.com"],  //PC端注册入口
+    regUrl_h5: ["http://www.gogo.com/h5","http://www.yb.com/h5"],  //H5端注册入口
     qq:["1234564","78946646"],  //QQ号码
     kefuLink_h5: "http://www.baidu.com", //H5端客服链接
     kefuLink_pc: "http://www.baidu.com", //PC端客服链接
